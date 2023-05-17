a072ecf3bf62d28671411f73989ab2555cb35ff4
3loomin <3loomin@gmail.com>
2023-05-17 19:36:05 +0900

    :sparkles: filter를 이용한 logging 기능 구현

---
+++ src/main/java/com/t3q/dranswer/filter/LoggingFilter.java
@@ -0,0 +1,88 @@
+package com.t3q.dranswer.filter;
+
+import com.t3q.dranswer.entity.LoggingFilterEntity;
+import com.t3q.dranswer.repository.LoggingFilterRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private final LoggingFilterRepository loggingFilterRepository;
+
+    @Autowired
+    public LoggingFilter(LoggingFilterRepository loggingfilterRepository) {
+        this.loggingFilterRepository = loggingfilterRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        String Headers = getHeaders(request).toString();
+        String queryString = getQueryParameter(request).toString();
+        String RequestBody = contentBody(requestWrapper.getContentAsByteArray());
+        String ResponseBody = contentBody(responseWrapper.getContentAsByteArray());
+
+        log.info("=======URI: [{}], METHOD: [{}]=======", request.getRequestURI(), request.getMethod());
+        log.info("Headers: {}", Headers);
+        log.info("QueryString: {}", queryString);
+        log.info("Request Body: {}", RequestBody);
+        log.info("Response Body: {}", ResponseBody);
+
+        LoggingFilterEntity filterEntity = new LoggingFilterEntity();
+        filterEntity.setHeader(Headers);
+        filterEntity.setQueryString(queryString);
+        filterEntity.setRequestBody(RequestBody);
+        filterEntity.setResponseBody(ResponseBody);
+
+        loggingFilterRepository.save(filterEntity);
+        responseWrapper.copyBodyToResponse();
+    }
+
+    private Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> headerMap = new HashMap<>();
+
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private Map<String, String> getQueryParameter(HttpServletRequest request) {
+        Map<String, String> queryMap = new HashMap<>();
+        request.getParameterMap()
+                .forEach((key, value) -> queryMap.put(key, String.join("", value)));
+        return queryMap;
+    }
+
+    private String contentBody(final byte[] contents) {
+        StringBuilder sb = new StringBuilder("\n");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(contents)));
+        bufferedReader.lines().forEach(str -> sb.append(str).append("\n"));
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+}
---
+++ src/main/java/com/t3q/dranswer/repository/LoggingFilterRepository.java
@@ -0,0 +1,7 @@
+package com.t3q.dranswer.repository;
+
+import com.t3q.dranswer.entity.LoggingFilterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoggingFilterRepository extends JpaRepository<LoggingFilterEntity, Long> {
+}
