d3442b53894a036b4f147b293657175780d98e6c
3loomin <3loomin@gmail.com>
2023-05-23 17:53:54 +0900

    :ghost: 오타 수정

--- src/main/java/com/t3q/dranswer/service/KeycloakService.java
+++ src/main/java/com/t3q/dranswer/service/KeycloakService.java
@@ -68,7 +68,7 @@ public class KeycloakService {
 		try {
 			ResponseEntity<KeycloakTokenRes> response = restTemplate.postForEntity(url, keycloakRequest, KeycloakTokenRes.class);
 			if (response.getStatusCode() == HttpStatus.OK) {
-				request.getSession().setAttribute(Constants.ACCESS_TOKEN_NAME, response.getBody().getAccessToken().replace('T', 'd'));
+				request.getSession().setAttribute(Constants.ACCESS_TOKEN_NAME, response.getBody().getAccessToken());
 				request.getSession().setAttribute(Constants.REFRESH_TOKEN_NAME, response.getBody().getRefreshToken());
 			}
 		} catch (Exception e) {
