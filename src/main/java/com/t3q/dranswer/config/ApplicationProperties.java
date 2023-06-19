42cf708822dfea337bc9864a11ad5dea5e81d6dc
junsul84 <junsul84@gmail.com>
2023-06-19 19:43:35 +0900

    .

--- src/main/java/com/t3q/dranswer/config/ApplicationProperties.java
+++ src/main/java/com/t3q/dranswer/config/ApplicationProperties.java
@@ -1,9 +1,8 @@
 package com.t3q.dranswer.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
 
 @Data
 @Component
